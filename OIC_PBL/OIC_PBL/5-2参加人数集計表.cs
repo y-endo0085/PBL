c1f98c1e74987e6bdecc20d70b0cfde531c675df
y-endo0085 <y.endo0085@gmail.com>
2021-06-30 14:04:58 +0900

    画面追加したぽよ～

---
+++ OIC_PBL/OIC_PBL/5-2参加人数集計表.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OIC_PBL
+{
+    public partial class _5_2参加人数集計表 : Form
+    {
+        public _5_2参加人数集計表()
+        {
+            InitializeComponent();
+        }
+    }
+}
