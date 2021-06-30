2676f5fec5f883e024b791e32261ccd8d1e96b23
Ishii4400 <77476119+Ishii4400@users.noreply.github.com>
2021-06-30 14:07:35 +0900

    画面変更

---
+++ OIC_PBL/OIC_PBL/4メニュー画面.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OIC_PBL
+{
+    public partial class _4メニュー画面 : Form
+    {
+        public _4メニュー画面()
+        {
+            InitializeComponent();
+        }
+    }
+}
