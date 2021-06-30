9ce77d940560d762aadd45eeb08f943e7ee92c6e
Ishii4400 <77476119+Ishii4400@users.noreply.github.com>
2021-06-30 13:44:07 +0900

    jjjj

--- OIC_PBL/OIC_PBL/コロナ対応画面1.Designer.cs
+++ OIC_PBL/OIC_PBL/コロナ対応画面1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // panel1
@@ -48,11 +49,21 @@
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(278, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(244, 183);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // コロナ対応画面1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Name = "コロナ対応画面1";
@@ -65,5 +76,6 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
