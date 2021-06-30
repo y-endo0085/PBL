795d79741bdef4f66695a32a49690fc3ad4e4451
Ishii4400 <77476119+Ishii4400@users.noreply.github.com>
2021-06-30 12:52:11 +0900

    test

--- OIC_PBL/OIC_PBL/コロナ対応画面1.Designer.cs
+++ OIC_PBL/OIC_PBL/コロナ対応画面1.Designer.cs
@@ -28,12 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(575, 108);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // コロナ対応画面1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "コロナ対応画面1";
             this.Text = "コロナ対応画面1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
     }
 }
