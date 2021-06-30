ca79324011c2dc8f5fd22bfa688afe3639e9ba81
Saison2457 <nishimura2457@gmail.com>
2021-06-30 13:06:00 +0900

    ｆｄｓio

--- OIC_PBL/OIC_PBL/.Designer.cs
+++ OIC_PBL/OIC_PBL/.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -51,11 +52,21 @@
             this.listBox1.Size = new System.Drawing.Size(74, 52);
             this.listBox1.TabIndex = 1;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(480, 300);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -69,6 +80,7 @@
         private System.Windows.Forms.Button button1;
 <<<<<<< HEAD
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
 =======
         private System.Windows.Forms.Panel panel1;
 >>>>>>> 2d4de7ce5023083cfbaeff36d8bf93936ff811da
